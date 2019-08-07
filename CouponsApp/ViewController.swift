98fca7b4c39dcbaaa09551ce8af4c410dced5c90
venco4 <venco4@sg.uob>
2019-08-07 11:06:31 +0800

    Initial Commit

---
+++ CouponsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CouponsApp
+//
+//  Created by venco4 on 7/8/19.
+//  Copyright © 2019 venco4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
